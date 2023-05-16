8ed1f03e679273a0c0660cebca7e7e9fbe4e5126
guzeradimaz <dguzerchuk@mail.ru>
2023-05-16 16:22:05 +0300

    Full commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+/* eslint-disable react/react-in-jsx-scope */
+import './App.css'
+import { LoginScreen } from './screens/LoginScreen/LoginScreen'
+import { useSelector } from 'react-redux'
+import { selectUser, setUser } from './features/userSlice'
+import { Messenger } from './screens/MessengerScreen/Messenger'
+import { selectSettings } from './features/settingsSlice'
+import { SettingsScreen } from './screens/SettingsScreen/SettingsScreen'
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import { removeChoosedThread } from './features/choosedThreadSlice'
+
+function App() {
+  const user = useSelector(selectUser)
+  const settings = useSelector(selectSettings)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    try {
+      const savedUser = localStorage.getItem('user')
+      const user = JSON.parse(savedUser)
+      dispatch(setUser(user))
+    } catch (error) {
+      console.log('[NO USERS IN LOCAL STORAGE]')
+    }
+    dispatch(removeChoosedThread(null))
+  }, [])
+
+  return (
+    <div className='App'>
+      {user.user ? (
+        settings.settings ? (
+          <SettingsScreen />
+        ) : (
+          <Messenger />
+        )
+      ) : (
+        <LoginScreen />
+      )}
+    </div>
+  )
+}
+
+export default App
